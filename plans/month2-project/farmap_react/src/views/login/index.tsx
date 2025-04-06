6ca8541fc4aab70858dbf3236f1401ce050b24cb
CHAM <450139220@qq.com>
2025-04-06 12:21:58 +0800

    feat(react): finish login function with no backend validation

--- plans/month2-project/farmap_react/src/views/login/index.tsx
+++ plans/month2-project/farmap_react/src/views/login/index.tsx
@@ -1,20 +1,61 @@
-import store from '@/store';
-import { login } from '../../store';
+import { FormEvent, useState } from 'react';
+import styles from './index.module.css';
+import store, { login } from '@/store';
 
+// logout
+// store.dispatch(login({ token: '', username: '', role: '' }));
 function Login() {
+	const [loginError, setLogginError] = useState(false);
+	const handleLogin = async (event: FormEvent) => {
+		// FIXME: how to restrict the parameter of <store.dispatch>?
+		event.preventDefault();
+		// TODO: connect to backend
+		const formData = new FormData(event.target as HTMLFormElement);
+		const username = formData.get('username');
+		const password = formData.get('password');
+		if (username === 'admin' && password === '123') {
+			store.dispatch(
+				login({ token: '123123', username: 'admin1', role: 'admin' })
+			);
+		} else {
+			setLogginError(true);
+			setTimeout(() => {
+				setLogginError(false);
+			}, 3000);
+		}
+		// TODO: connetc to backend
+	};
+
 	return (
 		<>
-			<div>LoginPage</div>
-			{<button onClick={handleLogin}>login</button>}
+			<form onSubmit={handleLogin}>
+				<div className="form-username">
+					<label htmlFor="form-username__input">username</label>
+					<input
+						type="text"
+						id="form-username__input"
+						name="username"
+						required
+					/>
+				</div>
+				<div className="form-password">
+					<label htmlFor="form-password__input">password</label>
+					<input
+						type="password"
+						id="form-password__input"
+						name="password"
+						required
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+			{loginError ? (
+				<div className={styles['error-tip']}>
+					username or passowrd not correct
+				</div>
+			) : null}
 		</>
 	);
-}
-
-async function handleLogin() {
-	// TODO: how to restrict the parameter of <store.dispatch>?
-	store.dispatch(login({ token: '123123', username: '123', role: 'ad' }));
-	// logout
-	// store.dispatch(login({ token: '', username: '', role: '' }));
 }
 
 export default Login;
