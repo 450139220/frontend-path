7b39528d683c82e374ab6bf07c03ed7d44e764ab
CHAM <450139220@qq.com>
2025-04-06 21:59:32 +0800

    feat(react): add some styles on layout

--- plans/month2-project/farmap_react/src/layout/index.tsx
+++ plans/month2-project/farmap_react/src/layout/index.tsx
@@ -5,19 +5,32 @@ import { routes } from '@/router/routes';
 function Layout() {
 	return (
 		<>
-			<div className="container">
-				<nav className="sidebar">
-					{routes.map((route, index) => (
-						<NavLink
-							className={({ isActive }) =>
-								isActive ? `${styles.navlink} ${styles.active}` : styles.navlink
-							}
-							key={index}
-							to={route.path}
-							end>
-							{route.name}
-						</NavLink>
-					))}
+			<div className={styles.container}>
+				<nav className={styles.nav}>
+					<div>
+						<div className={styles.logo}>
+							<img src="/" alt="logo" />
+							<span>智慧农业FarMap</span>
+						</div>
+						{routes.map((route, index) => (
+							<NavLink
+								className={({ isActive }) =>
+									isActive
+										? `${styles.navlink} ${styles.active}`
+										: styles.navlink
+								}
+								to={route.path}
+								key={index}
+								end>
+								<i className={`ri-${route.icon}-fill`}></i>
+								{route.name}
+							</NavLink>
+						))}
+					</div>
+					<div className={`${styles.navlink} ${styles.logout}`}>
+						<i className="ri-logout-box-r-line"></i>
+						退出登录
+					</div>
 				</nav>
 				<Outlet />
 			</div>
--- plans/month2-project/farmap_react/src/router/routes.ts
+++ plans/month2-project/farmap_react/src/router/routes.ts
@@ -3,6 +3,8 @@ import React, { JSX } from 'react';
 type CustomRoutes = {
 	name: string;
 	path: string;
+	// icon value is from remix icon
+	icon: string;
 	component: React.LazyExoticComponent<() => JSX.Element>;
 	children?: CustomRoutes;
 };
@@ -11,11 +13,13 @@ export const routes: CustomRoutes[] = [
 	{
 		name: '主页',
 		path: '/',
+		icon: 'git-repository',
 		component: React.lazy(() => import('@/views/home')),
 	},
 	{
 		name: '园区总览',
 		path: '/map',
+		icon: 'leaf',
 		component: React.lazy(() => import('@/views/map')),
 		// children: {
 		// 	name: '园区1',
@@ -26,11 +30,13 @@ export const routes: CustomRoutes[] = [
 	{
 		name: '综合展示',
 		path: '/dashboard',
+		icon: 'slideshow',
 		component: React.lazy(() => import('@/views/dashboard')),
 	},
 	{
 		name: '设备列表',
 		path: '/device',
+		icon: 'device',
 		component: React.lazy(() => import('@/views/device')),
 	},
 ];
