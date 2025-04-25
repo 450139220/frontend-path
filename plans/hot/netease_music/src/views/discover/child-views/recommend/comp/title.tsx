f19006218ea1c157bdc3d989019ec3635e601f18
CHAM <450139220@qq.com>
2025-04-25 16:48:47 +0800

    feat(music): add recommend box which includes title and content

---
+++ plans/hot/netease_music/src/views/discover/child-views/recommend/comp/title.tsx
@@ -0,0 +1,55 @@
+import styles from '../index.module.css';
+import type { JSX, ReactNode } from 'react';
+
+interface TitleProps {
+  name: string;
+  content?: string[];
+  to?: string;
+  children?: ReactNode;
+}
+
+function Title(props: TitleProps): JSX.Element {
+  return (
+    <>
+      <div className={styles['title-content']}>
+        {props.to ? (
+          <a className={styles['title-content__to']} href={props.to}>
+            {props.name}
+          </a>
+        ) : (
+          <div className={styles['title-content__to']}>{props.name}</div>
+        )}
+        {props.content ? (
+          <div className={styles['title-content__tab']}>
+            {props.content.map((item, index) => {
+              if (index === props.content!.length - 1) {
+                return (
+                  <div key={index}>
+                    <a href={`/#/discover/playlist?cat=${item}`}>{item}</a>
+                  </div>
+                );
+              }
+              return (
+                <div key={index}>
+                  <a href={`/#/discover/playlist?cat=${item}`}>{item}</a>
+                  <span>|</span>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+        {props.to && (
+          <div className={styles['title-content__more']}>
+            <a href="/#/discover/playlist">更多</a>
+            <i>&nbsp;</i>
+          </div>
+        )}
+      </div>
+      {props.children && (
+        <div className={styles['title-child']}>{props.children}</div>
+      )}
+    </>
+  );
+}
+
+export default Title;
--- plans/hot/netease_music/src/views/discover/child-views/recommend/index.tsx
+++ plans/hot/netease_music/src/views/discover/child-views/recommend/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import type { JSX } from 'react';
 import { request } from '@/service/request';
 import Carousel, { CarouselObject } from './comp/carousel';
+import Title from './comp/title';
 
 function Recommend(): JSX.Element {
   const [carouselData, setCarouselData] = useState<CarouselObject[]>([]);
@@ -15,6 +16,8 @@ function Recommend(): JSX.Element {
     });
   }, []);
 
+  const titleContent = ['华语', '流行', '摇滚', '民谣', '电子'];
+
   return (
     <>
       <Carousel
@@ -23,9 +26,24 @@ function Recommend(): JSX.Element {
         interval={5000}
         fadeTime={500}
       />
-      <div className={styles['content']}>
-        <div className={styles['content-l']}></div>
-        <div className={styles['content-r']}></div>
+      <div className={styles['content__bg']}>
+        <div className={styles['content']}>
+          <div className={styles['content-l']}>
+            <div className={styles['l-1']}>
+              <Title
+                name="热门推荐"
+                content={titleContent}
+                to="/#/discover/playlist"
+              >
+                <div>123</div>
+              </Title>
+              <Title name="个性化推荐"></Title>
+              <Title name="新碟上架" to="/#/discover/album"></Title>
+              <Title name="榜单" to="/#/discover/toplist"></Title>
+            </div>
+          </div>
+          <div className={styles['content-r']}></div>
+        </div>
       </div>
     </>
   );
