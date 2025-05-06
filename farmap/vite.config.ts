dca3a10613c81482fab92560eafd988e461cea38
CHAM <450139220@qq.com>
2025-05-06 15:04:22 +0800

    refactory(work): re-create work project again

--- farmap/vite.config.ts
+++ farmap/vite.config.ts
@@ -1,18 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  plugins: [react()],
+})
